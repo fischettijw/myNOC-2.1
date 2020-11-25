10c6a2135644a10813e8f7549b7838b22d7fa9c8
Joseph Fischetti <fischettijw@gmail.com>
2020-11-25 08:16:42 -0500

    added buttons

--- GravityAndWind.js
+++ GravityAndWind.js
@@ -2,6 +2,7 @@
 // Terminal Velocity       https://www.grc.nasa.gov/www/k-12/airplane/termv.html
 
 let t, diam, clr, mass, maxVel;
+let btnUp, btnDown, btnLeft, btnRight;
 
 function initialize() {
     initMyVariableNames();
@@ -9,11 +10,30 @@ function initialize() {
     clr = _clrAlmostRed;
     mass = 1;
     maxVel = 5;
+    createButtons();
+}
+
+function createButtons() {
+    btnUp = createButton("UP").position(width + 20, 10).style(`width: 60px; height: 28px`).id('UP');
+    document.getElementById('UP').onclick = function() { btnClicked('UP'); }
+    btnUp = createButton("DOWN").position(width + 20, 40).style(`width: 60px; height: 28px`).id('DOWN');
+    document.getElementById('DOWN').onclick = function() { btnClicked('DOWN'); }
+    btnUp = createButton("LEFT").position(width + 20, 70).style(`width: 60px; height: 28px`).id('LEFT');
+    document.getElementById('LEFT').onclick = function() { btnClicked('LEFT'); }
+    btnUp = createButton("RIGHT").position(width + 20, 100).style(`width: 60px; height: 28px`).id('RIGHT')
+    document.getElementById('RIGHT').onclick = function() { btnClicked('RIGHT'); }
+}
+
+function btnClicked(btn) {
+    if (btn == 'UP') { t.applyForce(createVector(0, -.001)); }
+    if (btn == 'DOWN') { t.applyForce(createVector(0, .001)); }
+    if (btn == 'LEFT') { t.applyForce(createVector(-.001, 0)); }
+    if (btn == 'RIGHT') { t.applyForce(createVector(.001, 0)); }
 }
 
 function setup() {
+    createCanvas(400, 400).position(10, 10);
     initialize();
-    createCanvas(460, 400).position(20, 20);
     let p = createVector(diam / 2, diam / 2);
     let v = createVector(2, 0);
     let a = createVector(0, 0.1);
@@ -25,11 +45,4 @@ function draw() {
     t.show();
     t.edges();
     t.update();
-}
-
-function keyPressed() {
-    if (keyCode === DOWN_ARROW) { t.applyForce(createVector(0, .001)); }
-    if (keyCode === UP_ARROW) { t.applyForce(createVector(0, -.001)); }
-    if (keyCode === RIGHT_ARROW) { t.applyForce(createVector(.001, 0)); }
-    if (keyCode === LEFT_ARROW) { t.applyForce(createVector(-.001, 0)); }
 }
